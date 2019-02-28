be72c77b9ced686be06f0185303064613e55ca79
Alejandro Ortiz <ortizalex.47@gmail.com>
2019-02-28 10:32:30 +0100

    Generado el tunel con ngrok, levantada la app y funcionando

--- apiTelegram.js
+++ apiTelegram.js
@@ -1,2 +1,18 @@
-let https = require('https')
+const Telegraf = require('telegraf')
+const express = require('express')
+const expressApp = express()
+
+const BOT_TOKEN = '691634425:AAHD2vJ0AjCfYs526Nb7jRz1QrUyb7Kft7E'
+
+const bot = new Telegraf(BOT_TOKEN)
+expressApp.use(bot.webhookCallback('/secret-path'))
+bot.telegram.setWebhook('https://0b94bf18.ngrok.io/secret-path')
+
+expressApp.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+expressApp.listen(3000, () => {
+  console.log('Example app listening on port 3000!')
+})
 
