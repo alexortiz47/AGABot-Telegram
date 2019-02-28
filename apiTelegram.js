e752d27371fa39acfef0c70de763501653656cc1
Alejandro Ortiz <ortizalex.47@gmail.com>
2019-02-28 16:48:57 +0100

    Funcionando que el usario meta la ciudad o direccion y controlando errores

--- apiTelegram.js
+++ apiTelegram.js
@@ -9,38 +9,56 @@ const BOT_TOKEN = '691634425:AAHD2vJ0AjCfYs526Nb7jRz1QrUyb7Kft7E'
 
 // Aquí generamos un nuevo bot (objeto Telegraf) con el token que nos da bot_father
 const bot = new Telegraf(BOT_TOKEN)
+
 // Le generamos los comandos que queramos. Hay que poner /hello
-bot.command('hello', (ctx) => ctx.reply('¡Hola! Soy AGABot🤖\n\n¿En qué puedo ayudarte?\n\nPara obtener ayuda introduce /help'))
+bot.command('hello', (ctx) => {
+    let nombre = ctx.message.from.first_name // Esto saca el nombre del usuario de la cuenta telegram desde donde se interactua con el bot
+    
+    return ctx.reply(`¡Hola ${nombre}! Soy AGABot 🤖\n\n¿En qué puedo ayudarte?\n\nPara obtener ayuda introduce /help`)
+})
 
 // Comando /HELP
 bot.command('help', (ctx) => ctx.reply('/hello - Inicia conversación\n\n/create - Muestra información sobre el equipo a cargo del proyecto\n\n/weather [tu ciudad] - te muestra la temperatura máxima, mínima para esa ciudad y el estado actual\n\n/whereami [tu direccion] - te devuelve la latitud y longitud de la dirección que especifiques en el comando'))
 
 // Comando /CREATE
-bot.command('create', (ctx) => ctx.replyWithPhoto(foto))
+bot.command('create', (ctx) => {
+    return ctx.replyWithPhoto(foto), ctx.reply('AGABot: by Ana, Gloria y Alex\nCopyright©-2019')
+})
 
-// Comando /WHEREAMI
+// Comando /WHEREAMI. 
 bot.command('whereami', (ctx) => {
-    let res = request('GET', 'https://geocode.xyz/madrid?json=1&auth=779248831305544628193x1897')
-    return ctx.reply(`Longitud: ${JSON.parse(res.getBody('utf8')).longt}°\n\nLatitud: ${JSON.parse(res.getBody('utf8')).latt}°`)
+    let msg = ctx.message.text.split(/\s(.+)/)[1] // Esto saca todo menos la primera palabra del texto (/wherami)
+
+        let res = request('GET', `https://geocode.xyz/${msg}?json=1&auth=779248831305544628193x1897`)
+
+        if(JSON.parse(res.getBody('utf8')).error){
+            return ctx.replyWithAnimation('https://media1.tenor.com/images/84e36470c3e534a5e71c208cd872d177/tenor.gif?itemid=6108221'), ctx.reply('Escribe más despacito...')
+        }else{
+            return ctx.reply(`Longitud: ${JSON.parse(res.getBody('utf8')).longt}°\n\nLatitud: ${JSON.parse(res.getBody('utf8')).latt}°`)
+        }
+
 })
 
 // Comando /WEATHER
 bot.command('weather', (ctx) => {
-    let res = request('GET', 'http://api.openweathermap.org/data/2.5/weather?q=Madrid&units=metric&APPID=167c7dd8e5dbfe65b6d448c20d4ef0e0')
-    return ctx.reply(`Temperatura actual: ${JSON.parse(res.getBody('utf8')).main.temp} ℃\n\nTemperatura mínima: ${JSON.parse(res.getBody('utf8')).main.temp_min} ℃\n\nTemperatura máxima: ${JSON.parse(res.getBody('utf8')).main.temp_max} ℃`)
+    let msg = ctx.message.text.split(/\s(.+)/)[1]
+    try{
+        let res = request('GET', `http://api.openweathermap.org/data/2.5/weather?q=${msg}&units=metric&APPID=167c7dd8e5dbfe65b6d448c20d4ef0e0`)
+
+        return ctx.reply(`🌡Temperatura actual: ${JSON.parse(res.getBody('utf8')).main.temp} ℃\n\n🌡Temperatura mínima: ${JSON.parse(res.getBody('utf8')).main.temp_min} ℃\n\n🌡Temperatura máxima: ${JSON.parse(res.getBody('utf8')).main.temp_max} ℃`)
+    }catch(err){
+        return ctx.replyWithAnimation('https://media1.tenor.com/images/ff7257ce7e22bb5e17eabda8123fb70e/tenor.gif?itemid=11019924'), ctx.reply('Anda anda, revisa bien la ciudad que has escrito...')
+    }
+    
 })
-
-
 
 expressApp.use(bot.webhookCallback('/secret-path'))
 bot.telegram.setWebhook('https://0b94bf18.ngrok.io/secret-path')
 
 expressApp.get('/tiempo', (req, res) => {
-        request(`http://api.openweathermap.org/data/2.5/weather?q=Madrid&units=metric&APPID=167c7dd8e5dbfe65b6d448c20d4ef0e0`, function(err, response, body){
-        if(err) return console.log(err.message)
-        res.json(JSON.parse(body).main)
-    })
+    
 })
+
 
 
 
